8e06b99f3bb5cfc4b644ace3bdff42357c6ce1de
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-06-03 10:05:59 +0530

    Tours Project Complete
    Description api call, use state, passing function, async calls, useeffect, passing object using spread operator

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,image,info,price,name,removeTour}) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+      <article className='single-tour'>
+        <image src={image} alt={name}/>
+        <footer>
+          <div className='tour-info'>
+            <h4>{name}</h4>
+            <h4 className='tour-price'>${price}</h4>
+          </div>
+          <img src={image}/>
+          <p>{
+          readMore ? info : `${info.substring(0,200)}...`
+          }
+          <button onClick={() => {setReadMore(!readMore)}}>{readMore ? 'slow less': 'read more'}</button>
+          </p>
+          <button className='delete-btn' onClick={() => {
+            removeTour(id);
+          }}>not interested</button>
+        </footer>
+      </article>
+  );
 };
 
 export default Tour;
