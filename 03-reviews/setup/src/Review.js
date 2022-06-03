1ecadad0f9dbea794ed7f7c969842855fb8aa445
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-06-03 11:27:50 +0530

    Reviews Project Complete
    Description learned how to use icons

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,41 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const {name,job,image,text} = people[index]
+
+  const nextPerson = () => {
+    setIndex((index) => {return (index+1)%people.length});
+  }
+  const prevPerson = () => {
+    setIndex((index-1 >= 0) ? index-1 : people.length-1 );
+  }
+  const randPerson = () => {
+    let nextNum = index;
+    while(nextNum === index && people.length > 1) nextNum = Math.floor(Math.random()*(people.length));
+    setIndex(nextNum);
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'/>
+        <span className='quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn' onClick={() => {prevPerson()}}><FaChevronLeft/></button>
+          <button className='prev-btn' onClick={() => {nextPerson()}}><FaChevronRight/></button>
+        </div>
+        <button className='random-btn' onClick={() => {randPerson()}}>Suprise me</button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
