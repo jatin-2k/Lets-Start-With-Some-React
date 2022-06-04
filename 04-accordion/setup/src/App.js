d43d335067a49b45656e0f57e49675de8548f78e
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-06-04 10:08:24 +0530

    Accordion Project Complete
    Description learned how to use states effectively

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import data from './data';
+import Question from './Question';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setOuestions] = useState(data);
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>Question and their answers</h3>
+        <section>
+        {
+          questions.map((question, index) => {
+            return (
+              <Question key={question.id} {...question}/>
+            )
+          })
+        }
+        </section>
+        </div>
+    </main>
+  );
 }
 
 export default App;
